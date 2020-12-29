fee879e379c2d9de534fdd0a44b0a43d2255e740
rkmod <63541190+rkmod@users.noreply.github.com>
2020-12-29 10:59:34 +0200

    Update mightygumball.js

--- MightyGumball/mightygumball.js
+++ MightyGumball/mightygumball.js
@@ -24,7 +24,10 @@ function updateSales(responseText) {
 }*/
     
     
+var lastReportTime = 0;
+
 window.onload = function() {
+    setInterval(handleRefresh, 3000);
 }
 
 function updateSales(salest) {
@@ -35,5 +38,27 @@ function updateSales(salest) {
         div.setAttribute("class", "saleItem");
         div.innerHTML = sale.name + " sold " + sale.sales + " gumballs";
         salesDiv.appendChild(div);
+    }
+    if (sales.length > 0) {
+        lastReportTime = sales[sales.length-1].time;
+    }
+}
+
+function handleRefresh() {
+    var url = "http://gumball.wickedlysmart.com" + 
+    "?callback=updateSales" + 
+    "&lastreporttime=" + lastReportTime +
+    "&random=" + (new Date()).getTime();
+
+    var newScriptElement = document.createElement("script");
+    newScriptElement.setAttribute("src", url);
+    newScriptElement.setAttribute("id", "jsonp");
+
+    var oldScriptElement = document.getElementById("jsonp");
+    var head = document.getElementsByTagName("head")[0];
+    if (oldScriptElement == null) {
+        head.appendChild(newScriptElement);
+    } else {
+        head.replaceChild(newScriptElement, oldScriptElement);
     }
 }
